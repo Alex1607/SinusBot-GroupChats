b03aaf86313ecfea9a285896b15208cf4e25d43d
Alexander <alex@pluoi.com>
2020-01-13 09:42:43 +0100

    Initial Commit

---
+++ groupchats.js
@@ -0,0 +1,14 @@
+///<reference path="node_modules/sinusbot/typings/global.d.ts" />
+
+registerPlugin({
+    "name": "GroupChats",
+    "description": "Allow users to create group chats",
+    "version": "1.0.0",
+    "author": "Kavatch",
+    "backends": [],
+    "vars": []
+  }, () => {
+  
+    
+  
+  })
