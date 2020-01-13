a7cbf7b6608d70cca36fd074d8532df2795409fb
Kavatch <alex@pluoi.com>
2020-01-13 21:01:57 +0100

    fixed some bugs; STILL NOT WORKING! Weird SinusBot bug

--- src/groupchats.ts
+++ src/groupchats.ts
@@ -1,8 +1,5 @@
 ///<reference path="../node_modules/sinusbot/typings/global.d.ts" />
-
 import { Client } from "../node_modules/sinusbot/typings/interfaces/Client";
-import { engine } from "../node_modules/sinusbot/typings/modules/engine";
-import { backend } from "../node_modules/sinusbot/typings/modules/backend";
 
 registerPlugin({
   "name": "GroupChats",
@@ -13,22 +10,24 @@ registerPlugin({
   "vars": []
 }, () => {
 
-  const { createCommand } = require("command");
+  const command = require("command")
+
   const event = require("event");
+  const engine = require("engine");
+  const backend = require("backend");
   const GROUPS: any = {};
   const GROUPUSERS: any = {};
 
   event.on("load", () => {
-    createCommand("creategroup")
+    command.createCommand("creategroup")
       .help("Creates a new group that anyone can join.")
       .manual("creategroup <name> creates a new group as long as the name does not yet exist. Everyone on the server can then join it.")
-      .exec((client, args, reply) => {
-        if (args.lenght() != 1) {
+      .addArgument(command.createArgument("string").setName("name"))
+      .exec((client, { name }, reply) => {
+        if (name == "") {
           reply("Incorrect usage. Please use: !creategroup <name>");
           return;
         }
-
-        let name = args[0];
 
         if (GROUPS[name] != undefined) {
           reply(`This group name is already in use. You can join the group with !joingroup ${name}`);
@@ -39,11 +38,18 @@ registerPlugin({
           "members": []
         };
 
+        if (GROUPUSERS[client.uid()] == undefined) {
+          GROUPUSERS[client.uid()] = {
+            "active": "",
+            "groups": []
+          }
+        }
+
         joingroup(client, name);
         reply(`Group ${name} was created successfully. Others can join with: !joingroup ${name}`);
       });
 
-    createCommand("joingroup")
+    command.createCommand("joingroup")
       .help("Join a group")
       .manual("joingroup [name] adds you to a group and makes this group the active chat group for you")
       .exec((client, args, reply) => {
@@ -59,10 +65,17 @@ registerPlugin({
           return;
         }
 
-        let groups: [string] = GROUPUSERS[client.uniqueId()].groups;
+        let groups: [any] = GROUPUSERS[client.uid()].groups;
         if (groups.includes(name)) {
           reply("You are already in this group.")
           return;
+        }
+
+        if (GROUPUSERS[client.uid()] == undefined) {
+          GROUPUSERS[client.uid()] = {
+            "active": "",
+            "groups": []
+          }
         }
 
         joingroup(client, name);
@@ -70,7 +83,7 @@ registerPlugin({
         sendGroupMessage(client, " has joined the group!");
       });
 
-    createCommand("leavegroup")
+    command.createCommand("leavegroup")
       .help("Leaves a group")
       .manual("leavegroup [name] will remove you from the group.")
       .exec((client, args, reply) => {
@@ -81,7 +94,7 @@ registerPlugin({
 
         let name = args[0];
 
-        if (!(GROUPUSERS[client.uniqueId()].groups).includes(name)) {
+        if (!(GROUPUSERS[client.uid()].groups).includes(name)) {
           reply(`No need to leave that group. You arent in this group.`);
           return;
         }
@@ -91,7 +104,7 @@ registerPlugin({
         sendGroupMessage(client, " has left the group!");
       });
 
-    createCommand("changegroup")
+    command.createCommand("changegroup")
       .help("Changes your active chat group.")
       .manual("Changes your active chat group. Your active chat group is the group in which your messages are displayed.")
       .exec((client, args, reply) => {
@@ -107,7 +120,7 @@ registerPlugin({
           return;
         }
 
-        let groups: [string] = GROUPUSERS[client.uniqueId()].groups;
+        let groups: [any] = GROUPUSERS[client.uid()].groups;
         if (!groups.includes(name)) {
           reply("You are not in in this group.")
           return;
@@ -118,54 +131,63 @@ registerPlugin({
       });
 
     event.on("chat", (event) => {
-      if (event.client == backend.getBotClient()) {
+      setTimeout(() => {
+        engine.log(JSON.stringify(event));
         return;
-      }
-      if ((event.text).startsWith(engine.getCommandPrefix())) {
-        return;
-      }
-      if (GROUPUSERS[event.client.uniqueId()].active != undefined) {
-        return;
-      }
-      sendGroupMessage(event.client, event.text);
+        if (event.client === backend.getBotClient()) {
+          engine.log("Bot");
+          return;
+        }
+        if ((event.text).startsWith(engine.getCommandPrefix())) {
+          engine.log("Prefix");
+          return;
+        }
+        if (GROUPUSERS[(event.client).uid()] == undefined) {
+          return;
+        }
+        if (GROUPUSERS[(event.client).uid()].active == undefined) {
+          return;
+        }
+        sendGroupMessage(event.client, event.text);
+      }, 500);
     });
 
     function joingroup(client: Client, groupname: string) {
-      (GROUPS[groupname].members).push(client.uniqueId())
+      (GROUPS[groupname].members).push(client.uid())
 
-      GROUPUSERS[client.uniqueId()].active = groupname;
-      (GROUPUSERS[client.uniqueId()].groups).push(groupname);
+      GROUPUSERS[client.uid()].active = groupname;
+      (GROUPUSERS[client.uid()].groups).push(groupname);
     }
 
     function leavegroup(client: Client, groupname: string) {
-      let groupsindex = (GROUPS[groupname].members).indexOf(client.uniqueId());
+      let groupsindex = (GROUPS[groupname].members).indexOf(client.uid());
       if (groupsindex > -1) {
         (GROUPS[groupname].members).splice(groupsindex, 1);
       }
 
-      GROUPUSERS[client.uniqueId()].active = undefined;
-      let usersindex = (GROUPUSERS[client.uniqueId()]).indexOf(groupname);
+      GROUPUSERS[client.uid()].active = undefined;
+      let usersindex = (GROUPUSERS[client.uid()]).indexOf(groupname);
       if (usersindex > -1) {
-        (GROUPUSERS[client.uniqueId()]).splice(usersindex, 1);
+        (GROUPUSERS[client.uid()]).splice(usersindex, 1);
       }
     }
 
     function setactive(client: Client, groupname: string) {
-      GROUPUSERS[client.uniqueId()].active = groupname;
+      GROUPUSERS[client.uid()].active = groupname;
     }
 
     function sendGroupMessage(client: Client, message: string) {
-      if (GROUPUSERS[client.uniqueId()].active == undefined) {
+      if (GROUPUSERS[client.uid()].active == undefined) {
         //TODO: Send Info message
         return false;
       }
-      let currentActiveGroup: string = GROUPUSERS[client.uniqueId()].active;
+      let currentActiveGroup: string = GROUPUSERS[client.uid()].active;
       let groupmembers: [string] = GROUPS[currentActiveGroup].members;
       groupmembers.forEach(member => {
-        if (member == client.uniqueId()) {
+        if (member == client.uid()) {
           return;
         }
-        backend.getClientByUniqueID(member).chat(`[${currentActiveGroup}] ${client.name()}: ${message}`);
+        backend.getClientByUID(member).chat(`[${currentActiveGroup}] ${client.name()}: ${message}`);
       });
       return true;
     }
